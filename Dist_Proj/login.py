ca239e7a6f2ccc60a647d07e49213fec7fe19bc2
Franky Chen <FrankyC495@gmail.com>
2016-02-18 20:37:08 -0500

    Fixed up readability

--- Dist_Proj/login.py
+++ Dist_Proj/login.py
@@ -1,27 +1,48 @@
-from flask import Flask, render_template, request
+from flask import Flask, session, redirect, url_for, escape, request, render_template
 
 app = Flask(__name__)
 
-# Note the addition of the methods parameter
+def login(username, password, fileOfUsers):
+	with open(fileOfUsers, "r") as fo:
+		for line in fo:
+			user = line.split()
+			if user[0] == username and user[1] == password: 	#Checks if username and pass matches
+				session['username'] = username
+				return redirect("/user")
+	return render_template("welcome.html", error = "Invalid Username or Password")
+
+def register(username, password, fileOfUsers):
+	with open(fileOfUsers,"a+") as fo:							#a+ is file descriptor for reading and appending
+		for line in fo:
+			user = line.split()
+			if user[0] == username:								#Checks if username is already taken
+				return render_template("login.html", error = "Username already taken")				
+		fo.write(username + ' ')								#Writes the value of username into the file
+		fo.write(password + ' ' + "\n") 						#Writers the value of password into file
+	return render_template("welcome.html")
+
+
 @app.route("/", methods=['post', 'get'])
-def someFunc():
+def home():
     if request.method=='POST':
 		if request.form["submit"] == "Login":
-			with open("test.txt", "r") as fo:
-				for line in fo:
-					user = line.split()
-					if user[0] == request.form["Username"] and user[1] == request.form["Password"]: #Checks if username and pass matches
-						return render_template("welcome.html")
-				return render_template("login.html", error = "Invalid Username or Password")
+			return login(request.form["Username"], request.form["Password"], "listOfUsers.txt")			
 		elif request.form["submit"] == "Register":
-			with open("test.txt","a+") as fo:					#a+ is file descriptor for reading and appending
-				for line in fo:
-					user = line.split()
-					if user[0] == request.form["Username"]:		#Checks if username is already taken
-						return render_template("login.html", error = "Invalid Username or Password")				
-	   			fo.write(request.form["Username"] + ' ')		#Writes the value of username into the file
-				fo.write(request.form["Password"] + ' ' + "\n") #Writers the value of password into file
-			return render_template("welcome.html")
+			return register(request.form["Username"], request.form["Password"], "listOfUsers.txt")
     return render_template("login.html")
 
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+@app.route("/user")
+def loadUserData():
+	return render_template("user.html")
+
+
+
+app.secret_key = "rj;elkjxia"
 app.run("127.0.0.1", 13000, debug=True)
